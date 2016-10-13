f864a10cfafcc7622846d3ff9802476a571f8c4d
koyowe <kevinoyowe@gmail.com>
2016-10-13 08:27:43 +0300

    Added version_control.py

---
+++ version_control.py
@@ -0,0 +1,13 @@
+# A list of contexts where version control is usefula and why
+
+Version control is the management of changes to a given collection of information
+
+In software engineering, version control is a practice that tracks and provides control over changes made to source code
+
+1. File locking - it is a method of preventing concurrent access problems. It can provide protection against difficult merge conflicts for example when a user is making changes to large parts of a file
+
+2. Version merging - This applies when several developers are working on a file at the same time
+
+3. Version Tracking - Helps in knowing the what, who, and when of changes. This helps while comparing the performance of particular versions to work when bugs were introduced or fixed 
+
+4. Coordinating Teams - version control allows one contributor to work on a copy of the resources and release release their changes back to the core when they are ready. Other contributors can also work on their own copies of the same resources at the same time, unaffected by each other's changes until they choose to merge or commit their changes back to the project.
